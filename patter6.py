a7bc2a16dcd05063a4b884545a35914406e7dccc
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2024-12-19 13:00:02 +0530

    Add files via upload

---
+++ patter6.py
@@ -0,0 +1,19 @@
+ev = []
+odd = []
+
+for i in range(8):
+    if i%2==0:
+        ev.append(i)
+    else:
+        odd.append(i)
+print(ev)
+print(odd)
+
+# for k in odd:
+#     for l in range(0,k):
+#         print("*",end="")
+#     print("")
+
+for k in range(4):
+    
+    print("")
