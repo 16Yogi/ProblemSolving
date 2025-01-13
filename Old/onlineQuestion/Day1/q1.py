fa1d8013854f3223c58268128567de7bf4e2ffb6
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2025-01-13 18:23:30 +0530

    Commit
    13125

---
+++ Old/onlineQuestion/Day1/q1.py
@@ -0,0 +1,9 @@
+# for i in range(1,10+1):
+    # print(i,end=" ")
+# 
+
+natural = 10
+i = 1
+while i<=natural:
+    print(i,end=" ")
+    i+=1
---
+++ Old/onlineQuestion/Day1/q2.py
@@ -0,0 +1,13 @@
+# for i in range(0,6+1):
+#     for j in range(1,i):
+#         print(j,end=" ")
+#     print("")
+
+i = 1
+j = 1
+while i<=5:
+    while j<i:
+        print(f"sdf:{j}",end=" ")
+        j+=1
+    print()
+    i+=1
