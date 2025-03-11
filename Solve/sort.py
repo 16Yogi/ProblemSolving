b580101583ae724abfb6332ab51a44a512cfefd6
Yogendra Meravi <85734470+16Yogi@users.noreply.github.com>
2025-03-11 08:17:46 +0530

    commit
    commit

--- Solve/sort.py
+++ Solve/sort.py
@@ -1,3 +1,6 @@
 arr = [12, 35, 1, 10, 34, 1]
-for i in arr:
-    
+for i in range(0,len(arr)):
+    for j in range(i+1,len(arr)):
+        if arr[i]>=arr[j]:
+            arr[i],arr[j] = arr[j],arr[i]
+print(arr)
---
+++ loop/q4.py
@@ -0,0 +1,4 @@
+num = int(input("enter number:"))
+for i in range(1,10+1):
+    # print(i*num)
+    print(f"{i} x {num} = {i*num}")
